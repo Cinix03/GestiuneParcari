ad7b9846c0ae3d705198fe1d1d862cc111807220
George Vasile <vasileirinel013@gmail.com>
2024-07-02 13:03:38 +0300

    Functia load_from_file + clasa de teste

--- src/Repositories/RepoParcare.java
+++ src/Repositories/RepoParcare.java
@@ -2,7 +2,11 @@ package Repositories;
 
 import Domain.Parcare;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class RepoParcare {
@@ -13,8 +17,18 @@ public class RepoParcare {
         this.nume_fisier = nume_fisier;
     };
 
-    private void load_from_file() {
+    private void load_from_file() throws IOException {
+        try{
+            List<String> lines = Files.readAllLines(Paths.get(nume_fisier));
+            for(String line : lines){
+                String[] parts = line.split(",");
+                Parcare p = new Parcare(parts[0], parts[1], Double.parseDouble(parts[2]), Integer.parseInt(parts[3]), Integer.parseInt(parts[4]), Integer.parseInt(parts[5]));
+                parcari.add(p);
+            }
 
+        }catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
     private void load_to_file(){
